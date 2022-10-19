03b3a9dd72180505f3cd8f5bf0b4191f0848c4db
Baleg00 <rona.balazs@gmail.com>
2022-10-19 13:50:19 +0200

    Stack adapter data structure.

---
+++ inc/stack.h
@@ -0,0 +1,16 @@
+#ifndef TAU_STACK_H
+#define TAU_STACK_H
+
+#include "list.h"
+
+typedef list_t stack_t;
+
+stack_t* stack_init(void);
+void stack_free(stack_t* stack);
+
+void stack_push(stack_t* stack, void* data);
+void* stack_pop(stack_t* stack);
+
+bool stack_empty(stack_t* stack);
+
+#endif
---
+++ src/stack.c
@@ -0,0 +1,26 @@
+#include "stack.h"
+
+stack_t* stack_init(void)
+{
+  return (stack_t*)list_init();
+}
+
+void stack_free(stack_t* stack)
+{
+  list_free((list_t*)stack);
+}
+
+void stack_push(stack_t* stack, void* data)
+{
+  list_push_back((list_t*)stack, data);
+}
+
+void* stack_pop(stack_t* stack)
+{
+  return list_pop_back((list_t*)stack);
+}
+
+bool stack_empty(stack_t* stack)
+{
+  return list_empty((list_t*)stack);
+}
