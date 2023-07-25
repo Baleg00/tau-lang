f84f75feef9c75ab45c14538c3dd76931deded6b
Róna Balázs <rona.balazs@gmail.com>
2023-07-25 15:20:05 +0200

    Comments and platform dependence of timer.
    - Added and modified comments.
    - Refactored platform specific implementations.

--- inc/timer.h
+++ inc/timer.h
@@ -1,7 +1,16 @@
 /**
- * \file
+ * \file timer.h
  * 
- * Timer library.
+ * \brief Timer library interface.
+ * 
+ * \details The timer library provides functionality to measure and track the
+ * elapsed time of code execution. It includes functions to retrieve the
+ * frequency of the system timer and to obtain the current timestamp. This
+ * allows developers to profile and optimize their code by identifying
+ * performance bottlenecks and measuring the impact of optimizations.
+ * 
+ * \copyright Copyright (c) 2023 Róna Balázs. All rights reserved.
+ * \license This project is released under the Apache 2.0 license.
 */
 
 #ifndef TAU_TIMER_H
@@ -12,19 +21,25 @@
 #include "log.h"
 
 /**
- * \brief Returns the frequency of the timer.
+ * \brief Returns the frequency of the timer in ticks per second.
  * 
- * \returns Timer frequency.
-*/
+ * \returns The frequency of the timer.
+ */
 uint64_t timer_freq(void);
 
 /**
- * \brief Returns the current timer counter.
+ * \brief Returns the current value of the timer in ticks.
  * 
- * \returns Timer counter.
-*/
+ * \returns The current value of the timer.
+ */
 uint64_t timer_now(void);
 
+/**
+ * \brief Measures the execution time of a given statement and logs the elapsed time in milliseconds.
+ *
+ * \param[in] NAME The name for identifying the measurement.
+ * \param[in] STMT The statement to be executed and timed.
+ */
 #define time_it(NAME, STMT)\
   do {\
     uint64_t time_begin_##NAME = timer_now();\
--- src/timer.c
+++ src/timer.c
@@ -1,36 +1,45 @@
+/**
+ * \file timer.c
+ * 
+ * \copyright Copyright (c) 2023 Róna Balázs. All rights reserved.
+ * \license This project is released under the Apache 2.0 license.
+*/
+
 #include "timer.h"
 
 #ifdef _WIN32
-
-#include <windows.h>
-#include <profileapi.h>
+# include <profileapi.h>
+# include <windows.h>
+#else
+# include <time.h>
+#endif
 
 uint64_t timer_freq(void)
 {
+#ifdef _WIN32
   LARGE_INTEGER f;
   QueryPerformanceFrequency(&f);
   return (uint64_t)f.QuadPart;
+#elif defined(__unix) || defined(__unix__) || defined(__gnu_linux__) || defined(__linux__)
+  struct timespec ts;
+  clock_getres(CLOCK_MONOTONIC, &ts);
+  return (uint64_t)(ts.tv_sec * 1000000ull + ts.tv_nsec);
+#else
+  return (uint64_t)CLOCKS_PER_SEC;
+#endif
 }
 
 uint64_t timer_now(void)
 {
+#ifdef _WIN32
   LARGE_INTEGER t;
   QueryPerformanceCounter(&t);
   return (uint64_t)t.QuadPart;
-}
-
+#elif defined(__unix) || defined(__unix__) || defined(__gnu_linux__) || defined(__linux__)
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return (uint64_t)(ts.tv_sec * 1000000ull + ts.tv_nsec);
 #else
-
-#include <time.h>
-
-uint64_t timer_freq(void)
-{
-  return (uint64_t)CLOCKS_PER_SEC;
-}
-
-uint64_t timer_now(void)
-{
   return (uint64_t)clock();
-}
-
 #endif
+}
