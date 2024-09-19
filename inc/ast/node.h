ffa5e536103a7e403699a32b607bb15a79379bf6
Róna Balázs <rona.balazs@gmail.com>
2024-09-19 16:02:42 +0200

    Fixed token and AST node kind documentation comments.

--- inc/ast/node.h
+++ inc/ast/node.h
@@ -30,74 +30,74 @@
  */
 typedef enum ast_kind_e
 {
-  AST_ID, // Identifier.
+  AST_ID, ///< Identifier.
 
-  AST_TYPE_ID, // Type identifier.
-  AST_TYPE_MUT, // Mutable type.
-  AST_TYPE_CONST, // Compile-time type.
-  AST_TYPE_PTR, // Pointer type.
-  AST_TYPE_ARRAY, // Array type.
-  AST_TYPE_REF, // Reference type.
-  AST_TYPE_OPT, // Optional type.
-  AST_TYPE_FUN, // Function type.
-  AST_TYPE_PRIM_I8, // Primitive type `i8`.
-  AST_TYPE_PRIM_I16, // Primitive type `i16`.
-  AST_TYPE_PRIM_I32, // Primitive type `i32`.
-  AST_TYPE_PRIM_I64, // Primitive type `i64`.
-  AST_TYPE_PRIM_ISIZE, // Primitive type `isize`.
-  AST_TYPE_PRIM_U8, // Primitive type `u8`.
-  AST_TYPE_PRIM_U16, // Primitive type `u16`.
-  AST_TYPE_PRIM_U32, // Primitive type `u32`.
-  AST_TYPE_PRIM_U64, // Primitive type `u64`.
-  AST_TYPE_PRIM_USIZE, // Primitive type `usize`.
-  AST_TYPE_PRIM_F32, // Primitive type `f32`.
-  AST_TYPE_PRIM_F64, // Primitive type `f64`.
-  AST_TYPE_PRIM_CHAR, // Primitive type `char`.
-  AST_TYPE_PRIM_BOOL, // Primitive type `bool`.
-  AST_TYPE_PRIM_UNIT, // Primitive type `unit`.
-  AST_TYPE_MEMBER, // Member type.
+  AST_TYPE_ID, ///< Type identifier.
+  AST_TYPE_MUT, ///< Mutable type.
+  AST_TYPE_CONST, ///< Compile-time type.
+  AST_TYPE_PTR, ///< Pointer type.
+  AST_TYPE_ARRAY, ///< Array type.
+  AST_TYPE_REF, ///< Reference type.
+  AST_TYPE_OPT, ///< Optional type.
+  AST_TYPE_FUN, ///< Function type.
+  AST_TYPE_PRIM_I8, ///< Primitive type `i8`.
+  AST_TYPE_PRIM_I16, ///< Primitive type `i16`.
+  AST_TYPE_PRIM_I32, ///< Primitive type `i32`.
+  AST_TYPE_PRIM_I64, ///< Primitive type `i64`.
+  AST_TYPE_PRIM_ISIZE, ///< Primitive type `isize`.
+  AST_TYPE_PRIM_U8, ///< Primitive type `u8`.
+  AST_TYPE_PRIM_U16, ///< Primitive type `u16`.
+  AST_TYPE_PRIM_U32, ///< Primitive type `u32`.
+  AST_TYPE_PRIM_U64, ///< Primitive type `u64`.
+  AST_TYPE_PRIM_USIZE, ///< Primitive type `usize`.
+  AST_TYPE_PRIM_F32, ///< Primitive type `f32`.
+  AST_TYPE_PRIM_F64, ///< Primitive type `f64`.
+  AST_TYPE_PRIM_CHAR, ///< Primitive type `char`.
+  AST_TYPE_PRIM_BOOL, ///< Primitive type `bool`.
+  AST_TYPE_PRIM_UNIT, ///< Primitive type `unit`.
+  AST_TYPE_MEMBER, ///< Member type.
 
-  AST_EXPR_ID, // Identifier expression.
-  AST_EXPR_LIT_INT, // Literal integer expression.
-  AST_EXPR_LIT_FLT, // Literal float expression.
-  AST_EXPR_LIT_STR, // Literal string expression.
-  AST_EXPR_LIT_CHAR, // Literal character expression.
-  AST_EXPR_LIT_BOOL, // Literal boolean expression.
-  AST_EXPR_LIT_NULL, // Literal null expression.
-  AST_EXPR_OP_UNARY, // Unary operation expression.
-  AST_EXPR_OP_BINARY, // Binary operation expression.
-  AST_EXPR_OP_CALL, // Call operation expression.
+  AST_EXPR_ID, ///< Identifier expression.
+  AST_EXPR_LIT_INT, ///< Literal integer expression.
+  AST_EXPR_LIT_FLT, ///< Literal float expression.
+  AST_EXPR_LIT_STR, ///< Literal string expression.
+  AST_EXPR_LIT_CHAR, ///< Literal character expression.
+  AST_EXPR_LIT_BOOL, ///< Literal boolean expression.
+  AST_EXPR_LIT_NULL, ///< Literal null expression.
+  AST_EXPR_OP_UNARY, ///< Unary operation expression.
+  AST_EXPR_OP_BINARY, ///< Binary operation expression.
+  AST_EXPR_OP_CALL, ///< Call operation expression.
 
-  AST_STMT_IF, // If statement.
-  AST_STMT_FOR, // For statement.
-  AST_STMT_WHILE, // While statement.
-  AST_STMT_DO_WHILE, //  Do-while statement.
-  AST_STMT_LOOP, //  Loop statement.
-  AST_STMT_BREAK, // Break statement.
-  AST_STMT_CONTINUE, // Continue statement.
-  AST_STMT_RETURN, // Return statement.
-  AST_STMT_DEFER, // Defer statement.
-  AST_STMT_BLOCK, // Block statement.
-  AST_STMT_EXPR, // Expression statement.
+  AST_STMT_IF, ///< If statement.
+  AST_STMT_FOR, ///< For statement.
+  AST_STMT_WHILE, ///< While statement.
+  AST_STMT_DO_WHILE, ///<  Do-while statement.
+  AST_STMT_LOOP, ///<  Loop statement.
+  AST_STMT_BREAK, ///< Break statement.
+  AST_STMT_CONTINUE, ///< Continue statement.
+  AST_STMT_RETURN, ///< Return statement.
+  AST_STMT_DEFER, ///< Defer statement.
+  AST_STMT_BLOCK, ///< Block statement.
+  AST_STMT_EXPR, ///< Expression statement.
 
-  AST_DECL_VAR, // Variable declaration.
-  AST_DECL_PARAM, // Parameter declaration.
-  AST_DECL_FUN, // Function declaration.
-  AST_DECL_STRUCT, // Struct declaration.
-  AST_DECL_UNION, // Union declaration.
-  AST_DECL_ENUM, // Enum declaration.
-  AST_DECL_ENUM_CONSTANT, // Enum constant declaration.
-  AST_DECL_MOD, // Module declaration.
+  AST_DECL_VAR, ///< Variable declaration.
+  AST_DECL_PARAM, ///< Parameter declaration.
+  AST_DECL_FUN, ///< Function declaration.
+  AST_DECL_STRUCT, ///< Struct declaration.
+  AST_DECL_UNION, ///< Union declaration.
+  AST_DECL_ENUM, ///< Enum declaration.
+  AST_DECL_ENUM_CONSTANT, ///< Enum constant declaration.
+  AST_DECL_MOD, ///< Module declaration.
 
-  AST_PATH_SEGMENT, // Path segment.
-  AST_PATH_ACCESS, // Path member access.
-  AST_PATH_LIST, // Path list.
-  AST_PATH_WILDCARD, // Path wildcard.
-  AST_PATH_ALIAS, // Path alias.
+  AST_PATH_SEGMENT, ///< Path segment.
+  AST_PATH_ACCESS, ///< Path member access.
+  AST_PATH_LIST, ///< Path list.
+  AST_PATH_WILDCARD, ///< Path wildcard.
+  AST_PATH_ALIAS, ///< Path alias.
 
-  AST_USE, // Use directive.
+  AST_USE, ///< Use directive.
 
-  AST_PROG, // Program.
+  AST_PROG, ///< Program.
 } ast_kind_t;
 
 /**
