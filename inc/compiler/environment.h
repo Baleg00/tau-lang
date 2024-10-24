5f7738c7247eeadd1342ef68f29f2b04a3a63f13
Róna Balázs <rona.balazs@gmail.com>
2024-10-24 10:19:50 +0200

    Added initial compilation environment implementation.

---
+++ inc/compiler/environment.h
@@ -0,0 +1,71 @@
+/**
+ * \file
+ *
+ * \brief Compilation environment.
+ *
+ * \copyright Copyright (c) 2023 Róna Balázs. All rights reserved.
+ * \license This project is released under the Apache 2.0 license.
+ */
+
+#ifndef TAU_ENVIRONMENT_H
+#define TAU_ENVIRONMENT_H
+
+#include "llvm.h"
+#include "stages/analysis/symtable.h"
+#include "stages/analysis/types/typebuilder.h"
+#include "stages/analysis/types/typetable.h"
+#include "utils/collections/vector.h"
+
+/**
+ * \brief Represents a compilation environment.
+ *
+ * \details The environment does not own any of its members. It is purely a
+ * convenience type that holds objects needed throughout the compilation
+ * process.
+ */
+typedef struct environment_t
+{
+  vector_t* paths;               ///< Vector of source file paths associated with the environment.
+  vector_t* sources;             ///< Vector of source file contents associated with the environment.
+  vector_t* tokens;              ///< Vector of tokens associated with the environment.
+
+  symtable_t* symtable;          ///< The symbol table associated with the environment.
+  typebuilder_t* typebuilder;    ///< The type builder associated with the environment.
+  typetable_t* typetable;        ///< The type table associated with the environment.
+
+  LLVMContextRef llvm_context;   ///< The LLVM context associated with the environment.
+  LLVMTargetDataRef llvm_layout; ///< The LLVM target data associated with the environment.
+  LLVMModuleRef llvm_module;     ///< The LLVM module associated with the environment.
+  LLVMBuilderRef llvm_builder;   ///< The LLVM IR builder associated with the environment.
+} environment_t;
+
+/**
+ * \brief Initializes a new compilation environment.
+ *
+ * \param[in] symtable Pointer to the symbol table to be used.
+ * \param[in] typebuilder Pointer to the type builder to be used.
+ * \param[in] typetable Pointer to the type table to be used.
+ * \param[in] llvm_context The LLVM context to be used.
+ * \param[in] llvm_layout The LLVM target data to be used.
+ * \param[in] llvm_module The LLVM module to be used.
+ * \param[in] llvm_builder The LLVM IR builder to be used.
+ */
+environment_t* environment_init(symtable_t* symtable, typebuilder_t* typebuilder, typetable_t* typetable, LLVMContextRef llvm_context, LLVMTargetDataRef llvm_layout, LLVMModuleRef llvm_module, LLVMBuilderRef llvm_builder);
+
+/**
+ * \brief Frees all resources associated with a compilation environment.
+ *
+ * \param[in] env Pointer to the environment to be freed.
+ */
+void environment_free(environment_t* env);
+
+/**
+ * \brief Merges a compilation environment into another. The source environment
+ * is freed in the process.
+ *
+ * \param[in,out] dest Pointer to the compilation environment to merge into.
+ * \param[in] src Pointer to the compilation environment to be merged.
+ */
+void environment_merge(environment_t* dest, environment_t* src);
+
+#endif
---
+++ src/compiler/environment.c
@@ -0,0 +1,51 @@
+/**
+ * \file
+ *
+ * \copyright Copyright (c) 2023 Róna Balázs. All rights reserved.
+ * \license This project is released under the Apache 2.0 license.
+ */
+
+#include "compiler/environment.h"
+
+#include "utils/common.h"
+#include "utils/memory/memtrace.h"
+
+environment_t* environment_init(symtable_t* symtable, typebuilder_t* typebuilder, typetable_t* typetable, LLVMContextRef llvm_context, LLVMTargetDataRef llvm_layout, LLVMModuleRef llvm_module, LLVMBuilderRef llvm_builder)
+{
+  environment_t* env = (environment_t*)malloc(sizeof(environment_t));
+  ASSERT(env != NULL);
+
+  env->paths = vector_init();
+  env->sources = vector_init();
+  env->tokens = vector_init();
+  env->symtable = symtable;
+  env->typebuilder = typebuilder;
+  env->typetable = typetable;
+  env->llvm_context = llvm_context;
+  env->llvm_layout = llvm_layout;
+  env->llvm_module = llvm_module;
+  env->llvm_builder = llvm_builder;
+
+  return env;
+}
+
+void environment_free(environment_t* env)
+{
+  vector_free(env->paths);
+  vector_free(env->sources);
+  vector_free(env->tokens);
+  free(env);
+}
+
+void environment_merge(environment_t* dest, environment_t* src)
+{
+  symtable_merge(dest->symtable, src->symtable);
+  typetable_merge(dest->typetable, src->typetable);
+
+  if (LLVMLinkModules2(dest->llvm_module, src->llvm_module))
+  {
+    log_error("env", "Failed to link modules.");
+  }
+
+  environment_free(src);
+}
